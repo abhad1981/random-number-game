89ad4d0bcc922b333cfb889187a42ea56e625f05
abhad <117444348+abhad1981@users.noreply.github.com>
2022-11-04 12:50:01 +0000

    wrote main function

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import random
 
+num = 0
 def random_number_guess():
   random_num = random.randint(0,10)
   return random_num
 
-
+def difference_from_answer(guess, answer = random_number_guess()):
+  guess = input("what is your guess")
+  if guess>answer:
+    print("too high")
+  elif guess<answer:
+    print("too low")
+  else: print("correct")
+  
+  
 
